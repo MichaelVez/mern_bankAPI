f0894c88e242d66e13826318c8b1be2e64527ce5
MichaelVez <michaelx979@gmail.com>
2022-06-25 18:19:06 +0300

    fix

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+let url = "http://localhost:5050/user";
+if (process.env.NODE_ENV === "production") {
+  url = "/user";
+}
 export const userAPI = axios.create({
-  baseURL: "http://localhost:5050/user",
+  baseURL: url,
 });
